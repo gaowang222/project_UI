d1c7b2236dd87c15a1986ef7e099fe45b652b77d
gaowang222 <gaowang0724@qq.com>
2017-11-20 09:32:43 +0800

    add

--- application/models/indexModel.php
+++ application/models/indexModel.php
@@ -17,18 +17,23 @@ class indexModel extends CI_Model
         $query = $this->db->query($sql);
         return $query->result_array();
     }
+    function getItem(){
+        $sql = "select * from item ";
+        $query = $this->db->query($sql);
+        return $query->result_array();
+    }
     function getSlider($table,$aimg){
         $sql = "select item_id,item_name,imgs from $table where aimg = $aimg";
         $query = $this->db->query($sql);
         return $query->result_array();
     }
-    function getFirstProject($table){
-        $sql="select parent_cate_id,cate_name from $table where  parent_cate_id = 2 or  parent_cate_id = 3 or  parent_cate_id = 4";
+    function getFirstProject(){
+        $sql="select parent_cate_id,cate_name from cate where  parent_cate_id = 2 or  parent_cate_id = 3 or  parent_cate_id = 4";
         $query = $this->db->query($sql);
         return $query->result_array();
     }
     function getFirstProject2($table){
-        $sql="select parent_cate_id,cate_name,status from $table where  status = 2 or  status = 3 or  status = 4";
+        $sql="select parent_cate_id,cate_name,status,cate_id from $table where  status = 2 or  status = 3 or  status = 4";
         $query = $this->db->query($sql);
         return $query->result_array();
     }
