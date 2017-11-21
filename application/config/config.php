1b25f03f6a4a944af0459fb0c5fe0eb062da52f1
lulu116 <sliusting970@163.com>
2017-11-21 09:57:46 +0800

    修改项目

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.4.121:80/project_UI';
+$config['base_url'] = 'http://localhost:80/project_UI';
 
 /*
 |--------------------------------------------------------------------------
